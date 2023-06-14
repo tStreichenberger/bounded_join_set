6c709c8027326914f4c07c4f6538fb6f78999e55
tStreichenberger <tristan@thehive.ai>
2023-06-14 10:45:31 -0700

    can't use channel for nonsend T

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -4,16 +4,35 @@ use std::pin::Pin;
 use tokio::sync::mpsc;
 use tokio::task::JoinSet as TokioJoinSet;
 
-pub struct DispatcherRequest<T> {
-    fut: Pin<Box<dyn Future<Output = T> + Send + 'static>>,
+
+pub enum DispatcherRequest<T> {
+    Local {
+        fut: Pin<Box<dyn Future<Output = T> + 'static>>,
+    },
+    NonLocal {
+        fut: Pin<Box<dyn Future<Output = T> + Send + 'static>>,
+    }
 }
 
+// pub struct DispatcherRequest<T> {
+//     fut: Pin<Box<dyn Future<Output = T> + Send + 'static>>,
+// }
+
 impl<T> DispatcherRequest<T> {
-    pub fn new<F>(task: F) -> Self
+    pub fn non_local<F>(task: F) -> Self
     where
         F: Future<Output = T> + Send + 'static,
     {
-        Self {
+        Self::NonLocal {
+            fut: Box::pin(task),
+        }
+    }
+
+    pub fn local<F>(task: F) -> Self
+    where
+        F: Future<Output = T> + 'static,
+    {
+        Self::Local {
             fut: Box::pin(task),
         }
     }
@@ -35,7 +54,7 @@ pub struct Dispatcher<T> {
 // this is going to get much more complicated with that
 //
 // maybe put the dispatch function as a method of the request and make the request an enum based on the response type
-impl<T: 'static + Send> Dispatcher<T> {
+impl<T: 'static> Dispatcher<T> {
     pub async fn start(mut self) {
         while let Some(request) = self.request_receiver.recv().await {
             // check concurrency
@@ -48,18 +67,48 @@ impl<T: 'static + Send> Dispatcher<T> {
         }
     }
 
-    pub fn dispatch(&mut self, request: DispatcherRequest<T>) {
+    fn dispatch(&mut self, request: DispatcherRequest<T>) {
         let responder = self.response_sender.clone();
 
-        let task = async move {
-            let payload = request.fut.await;
+        match request {
+            DispatcherRequest::NonLocal { fut } => {
+                let task = async move {
+                    let payload = fut.await;
+                    let response = DispatcherResponse{payload};
+                    // TODO: confirm that ignoring this error is okay
+                    _ = responder.send(response);
+                };
+                self.join_set.spawn(task);
+            }
+            DispatcherRequest::Local { fut } => {
+                let task = async move {
+                    let payload = fut.await;
+                    let response = DispatcherResponse{payload};
+                    // TODO: confirm that ignoring this error is okay
+                    _ = responder.send(response);
+                };
+                self.join_set.spawn_local(task);
+            }
+        }
 
-            let response = DispatcherResponse { payload };
-            // TODO: confirm that ignoring this error is okay
-            _ = responder.send(response);
-        };
+        // let task = async move {
+        //     let payload = match request {
+        //         DispatcherRequest::Local { fut } => fut.await,
+        //         DispatcherRequest::NonLocal { fut } => fut.await,
+        //     };
 
-        // TODO: do something with the abort handle from this. Perhaps inner hash map in JoinSet
-        self.join_set.spawn(task);
+        //     let response = DispatcherResponse { payload };
+        //     // TODO: confirm that ignoring this error is okay
+        //     _ = responder.send(response);
+        // };
+
+        // // let task = async move {
+        // //     let payload = request.fut.await;
+
+            
+        // // };
+
+        // // TODO: do something with the abort handle from this. Perhaps inner hash map in JoinSet
+        // self.join_set.spawn(task);
     }
 }
